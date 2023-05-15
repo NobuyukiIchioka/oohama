30a9d5779d0b2419c91a0d2606248e6737faa6df
NI <75564919+NobuyukiIchioka@users.noreply.github.com>
2023-05-15 09:31:34 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,36 @@
 // header
     // 動画コントローラー
     // ビデオ、アイコン画像、ボリュームを取得
-    const video = document.querySelector("video");
+    const videos = document.querySelectorAll("video");
     const mute_icon = document.getElementById("icon");
     const volume_slider = document.getElementById("volume");
     
-    // ボリュームの初期設定
-    video.volume = volume_slider.value;
+    // ボリュームの初期設定、PCとSPの両方のvideoタグの情報を認識できる設定
+    videos.forEach(video => {
+        video.volume = volume_slider.value;
+    });
+
     // 消音ボタンの画像を切り替え
     mute_icon.onclick = function() {
-        if(video.muted){
-            video.muted = false;
-            icon.src = "./img/volume-high.svg";
-        }else{
-            video.muted = true;
-            icon.src = "./img/volume-mute.svg";
-        }
+        videos.forEach(video => {
+            if(video.muted){
+                video.muted = false;
+                icon.src = "./img/volume-high.svg";
+            }else{
+                video.muted = true;
+                icon.src = "./img/volume-mute.svg";
+            }
+        });
     }
+
     // 音量調整スライダーを操作したとき
     volume_slider.addEventListener("input", (e) => {
-        video.volume = volume_slider.value;
-      });
-
-    // 音楽コントローラー
-    // 音楽、再生画像、音量画像、ボリュームを取得
-//     const music = document.getElementById("music");
-//     const play = document.getElementById("play");
-//     const icon2 = document.getElementById("icon2");
-
-//     function toggleAudio() {
-//         if (music.paused) {
-//             music.play();
-//             play.src = "./img/play.svg";
-//         } else {
-//             music.pause();
-//             play.src = "./img/pause.svg";
-//         }
-//     }
-
-//     function toggleMute() {
-//     if (music.muted) {
-//         music.muted = false;
-//         icon2.src = "./img/volume-high.svg";
-//     } else {
-//         music.muted = true;
-//         icon2.src = "./img/volume-mute.svg";
-//     }
-//   }
-
+        videos.forEach(video => {
+            video.volume = volume_slider.value;
+        });
+    });
+ 
+   
 
 
 /* 2ndーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー */
