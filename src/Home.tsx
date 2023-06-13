bea294c67eb2bc1cadaede467c1b4e07d40d0051
YutaCurry <42960955+YutaCurry@users.noreply.github.com>
2023-06-14 02:20:08 +0900

    react-router add

---
+++ src/Home.tsx
@@ -0,0 +1,25 @@
+export function Home() {
+	return (
+		<main>
+			<div className="wrapper1 body">
+				<div className="title">
+					<a href="./1st-2.html">
+						<p>
+							<img src="./img/bt1.png" alt="button" className="btn1" />
+						</p>
+						<p className="blinking">
+							<img src="./img/caution.png" alt="注意喚起" className="caution" />
+						</p>
+						<p className="blinking">
+							<img
+								src="./img/caution_sp.png"
+								alt="注意喚起"
+								className="caution_sp"
+							/>
+						</p>
+					</a>
+				</div>
+			</div>
+		</main>
+	)
+}
