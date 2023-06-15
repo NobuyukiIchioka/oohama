e26dfbfe24a93a65b02a8c4028a9a0d748c59669
YutaCurry <42960955+YutaCurry@users.noreply.github.com>
2023-06-16 07:26:27 +0900

    modify

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { First, FirstSub, Second } from "./page"
+import { First, Second } from "./page"
 
 const router = createBrowserRouter([
 	{
